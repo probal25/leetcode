74aec5d74d762ccd743a25013412c42024832a3b
probal <probal25@hotmail.com>
2022-08-14 01:20:41 +0600

    merge_two_list updated with recursive solution.

--- src/easy/merge_two_sorted_list/Solution.java
+++ src/easy/merge_two_sorted_list/Solution.java
@@ -18,19 +18,17 @@ class ListNode {
 
 class Solution {
 
-    public static void main(String[] args) {
-        ListNode listNode = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        listNode.next = listNode2;
-        ListNode newList = mergeTwoLists(listNode, listNode2);
-//        System.out.println(mergeTwoLists(listNode, listNode2));
+    // recursive
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
 
-        while (newList.next != null) {
-            System.out.println(newList.val);
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-    }
-
-    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
     }
 }
---
+++ src/easy/reverse_a_linked_list/Solution.java
@@ -0,0 +1,38 @@
+package easy.reverse_a_linked_list;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next;
+
+        if (head == null) return null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
